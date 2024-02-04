92412a586bcd1a21d5cb82e23877b1e8a057e979
widal001 <williamdaly422@gmail.com>
2024-02-04 13:56:22 -0500

    feat: Adds root endpoint `/` that welcomes user

--- src/cofundable/api.py
+++ src/cofundable/api.py
@@ -3,6 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/")
+async def root():
+    """Welcome user to the API and direct to openAPI spec."""
+    return {
+        "status": "ok",
+        "message": (
+            "Welcome to the Cofundable API. "
+            "Visit /docs for more information about the API resources available."
+        ),
+    }
+
+
 @app.get("/health-check")
 async def health_check():
     """Check that the API is available."""
