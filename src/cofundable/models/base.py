a8640381d53928089a2e1191acb6177f15ac9caf
widal001 <williamdaly422@gmail.com>
2024-02-04 23:31:57 -0500

    feat: Adds UUIDAuditBase class Creates a base db model that includes id, created_at, and updated_at

---
+++ src/cofundable/models/base.py
@@ -0,0 +1,23 @@
+"""Create base models that other models can inherit from."""
+
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import func
+
+
+class UUIDAuditBase(DeclarativeBase):
+    """Base db model that includes id, created_at, and update_at."""
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    created_at = mapped_column(
+        DateTime(timezone=True),
+        default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=func.now(),
+        onupdate=func.now(),
+    )
