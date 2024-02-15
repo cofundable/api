6c563e02f7db79b65de624808db95a4aed32e79c
widal001 <williamdaly422@gmail.com>
2024-02-14 22:31:21 -0500

    test: Adds utilities to populate the db with test data

---
+++ tests/utils/test_data.py
@@ -0,0 +1,46 @@
+"""Create test data for the mock database."""
+
+from uuid import uuid4, uuid5
+
+namespace = uuid4()
+
+# causes
+ACME = uuid5(namespace, "Acme")
+MUTUAL_AID = uuid5(namespace, "mutual aid")
+CAUSES = {
+    ACME: {
+        "id": ACME,
+        "name": "Acme",
+        "description": "Local organization",
+    },
+    MUTUAL_AID: {
+        "id": MUTUAL_AID,
+        "name": "Mutual aid group",
+        "description": "Local organization",
+    },
+}
+
+# tags
+TAG_A = uuid5(namespace, "a")
+TAG_B = uuid5(namespace, "b")
+TAG_C = uuid5(namespace, "c")
+TAGS = {
+    TAG_A: {
+        "id": TAG_A,
+        "name": "a",
+    },
+    TAG_B: {
+        "id": TAG_B,
+        "name": "b",
+    },
+    TAG_C: {
+        "id": TAG_C,
+        "name": "c",
+    },
+}
+
+# cause tags
+CAUSE_TAGS = {
+    ACME: [TAG_A, TAG_B],
+    MUTUAL_AID: [TAG_B, TAG_C],
+}
