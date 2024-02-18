efc79b9fe60c5cf11cdf181d022edece65cea1cf
widal001 <williamdaly422@gmail.com>
2024-02-18 01:32:16 -0500

    feat: Sets up alembic and adds first migration script

--- src/cofundable/models/__init__.py
+++ src/cofundable/models/__init__.py
@@ -1,1 +1,16 @@
-"""Create the database models for the Cofundable API resources."""
+"""
+Create the database models for the Cofundable API resources.
+
+Note:
+----
+All of the models need to be imported here in order to be registered by Alembic,
+so that the migration scripts are auto-generated correctly
+
+
+"""
+
+__all__ = ["UUIDAuditBase", "Cause", "Tag"]
+
+from cofundable.models.base import UUIDAuditBase
+from cofundable.models.cause import Cause
+from cofundable.models.tag import Tag
