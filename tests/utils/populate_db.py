2334386e9e0e51fd1300c64416aa5185b4c2d25d
widal001 <williamdaly422@gmail.com>
2024-02-19 13:00:07 -0500

    refactor: Test data in populate_db.py Makes it easier to add new test data without requiring changes to the populate_db() function

--- tests/utils/populate_db.py
+++ tests/utils/populate_db.py
@@ -2,9 +2,34 @@
 
 from sqlalchemy.orm import Session
 
-from cofundable.models.cause import Cause
-from cofundable.models.tag import Tag
+from cofundable.models.base import UUIDAuditBase
 from tests.utils import test_data as data
+
+
+def create_records_and_add_to_session(
+    model: type[UUIDAuditBase],
+    records: dict,
+    session: Session,
+) -> dict[str, UUIDAuditBase]:
+    """
+    Create records with the data and model provided and add them to the session.
+
+    Parameters
+    ----------
+    model: type[UUIDAuditBase]
+        A subclass of UUIDAuditBase model to use to create the record
+    records: list[TestRecord]
+        A list of items that map the record id to the record's input data
+    session:
+        A SQLAlchemy session that will commit the resulting records to the db
+
+    """
+    record_map = {}
+    for record_id, test_data in records.items():
+        record = model(id=record_id, **test_data)
+        record_map[record_id] = record
+        session.add(record)
+    return record_map
 
 
 def populate_db(session: Session) -> None:
@@ -17,21 +42,21 @@ def populate_db(session: Session) -> None:
         A SQLAlchemy session object passed to this function by the fixture
 
     """
-    # init causes and tags
-    causes = {id_: Cause(**fields) for id_, fields in data.CAUSES.items()}
-    tags = {id_: Tag(**fields) for id_, fields in data.TAGS.items()}
+    # populate UUID-based tables
+    records = {}
+    for table, test_data in data.UUID_TABLES.items():
+        records[table] = create_records_and_add_to_session(
+            session=session,
+            model=test_data.model,
+            records=test_data.records,
+        )
 
     # associate tags with causes
     for cause_id, tag_ids in data.CAUSE_TAGS.items():
-        cause = causes.get(cause_id)
-        if not cause:
-            continue
+        cause = records["cause"][cause_id]
         for tag_id in tag_ids:
-            tag = tags.get(tag_id)
-            if tag:
-                cause.tags.add(tag)
+            tag = records["tag"][tag_id]
+            cause.tags.add(tag)
 
-    # add records to session and then commit
-    session.add_all(tags.values())
-    session.add_all(causes.values())
+    # commit the changes
     session.commit()
--- tests/utils/test_data.py
+++ tests/utils/test_data.py
@@ -1,21 +1,37 @@
 """Create test data for the mock database."""
 
-from uuid import uuid4, uuid5
+from dataclasses import dataclass
+from uuid import UUID, uuid4, uuid5
+
+from cofundable.models import (
+    UUIDAuditBase,
+    Bookmark,
+    Cause,
+    Tag,
+    User,
+)
 
 namespace = uuid4()
+
+
+@dataclass
+class MockTableData:
+    """Map test data to the model used to populate that test data in the db."""
+
+    model: type[UUIDAuditBase]
+    records: dict[UUID, dict]
+
 
 # causes
 ACME = uuid5(namespace, "Acme")
 MUTUAL_AID = uuid5(namespace, "mutual aid")
 CAUSES = {
     ACME: {
-        "id": ACME,
         "name": "Acme",
         "handle": "acme",
         "description": "Local organization",
     },
     MUTUAL_AID: {
-        "id": MUTUAL_AID,
         "name": "Mutual aid group",
         "handle": "mutual-aid",
         "description": "Local organization",
@@ -27,22 +43,51 @@ TAG_A = uuid5(namespace, "a")
 TAG_B = uuid5(namespace, "b")
 TAG_C = uuid5(namespace, "c")
 TAGS = {
-    TAG_A: {
-        "id": TAG_A,
-        "name": "a",
-    },
-    TAG_B: {
-        "id": TAG_B,
-        "name": "b",
-    },
-    TAG_C: {
-        "id": TAG_C,
-        "name": "c",
-    },
+    TAG_A: {"name": "a"},
+    TAG_B: {"name": "b"},
+    TAG_C: {"name": "c"},
 }
 
 # cause tags
 CAUSE_TAGS = {
     ACME: [TAG_A, TAG_B],
     MUTUAL_AID: [TAG_B, TAG_C],
+}
+
+
+# users
+ALICE = uuid5(namespace, "alice")
+BOB = uuid5(namespace, "bob")
+USERS = {
+    ALICE: {
+        "username": "alice",
+        "name": "Alice Williams",
+        "bio": "Bio for Alice",
+    },
+    BOB: {
+        "username": "bob",
+        "name": "Bob Johnson",
+    },
+}
+
+# bookmarks
+ALICE_ACME = uuid5(namespace, "alice<>acme")
+BOB_ACME = uuid5(namespace, "bob<>acme")
+BOOKMARKS = {
+    ALICE_ACME: {
+        "cause_id": ACME,
+        "user_id": ALICE,
+    },
+    BOB_ACME: {
+        "cause_id": ACME,
+        "user_id": ALICE,
+    },
+}
+
+# records to create and insert directly
+UUID_TABLES = {
+    "cause": MockTableData(model=Cause, records=CAUSES),
+    "tag": MockTableData(model=Tag, records=TAGS),
+    "user": MockTableData(model=User, records=USERS),
+    "bookmark": MockTableData(model=Bookmark, records=BOOKMARKS),
 }
