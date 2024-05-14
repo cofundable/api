e604eda3534748bd2f8a111e4c3a989c3a5fe478
widal001 <williamdaly422@gmail.com>
2024-05-13 21:11:19 -0400

    test: Reorders tests to confirm session is rolled back

--- tests/unit_tests/services/test_users.py
+++ tests/unit_tests/services/test_users.py
@@ -42,6 +42,34 @@ class TestGetUserByUsername:
         assert result is None
 
 
+class TestDelete:
+    """Test the CauseCRUD.delete() method."""
+
+    def test_delete_cascades_to_bookmarks_but_not_causes(
+        self,
+        test_session: Session,
+    ):
+        """Deleting a cause should delete associated bookmarks, but not users."""
+        # set up - confirm alice exists
+        alice_id = uuid5(NAMESPACE, "alice")
+        assert test_session.get(User, alice_id) is not None
+        # setup - confirm cause exists
+        acme_id = uuid5(NAMESPACE, "acme")
+        assert test_session.get(Cause, acme_id) is not None
+        # set up - confirm bookmarks exist
+        bookmark_query = select(Bookmark).where(Bookmark.user_id == alice_id)
+        bookmarks_before = test_session.execute(bookmark_query).scalars().all()
+        assert len(bookmarks_before) == 1
+        # execution
+        user_service.delete(test_session, row_id=alice_id)
+        # validation - confirm user and bookmarks were deleted
+        bookmarks_after = test_session.execute(bookmark_query).scalars().all()
+        assert test_session.get(User, alice_id) is None
+        assert len(bookmarks_after) == 0
+        # validation - confirm cause was NOT deleted
+        assert test_session.get(Cause, acme_id) is not None
+
+
 class TestUpdate:
     """Tests the CauseCRUD.update() method."""
 
@@ -84,31 +112,3 @@ class TestUpdate:
                 assert alice_new[field] == alice_old[field]
             else:
                 assert alice_new[field] == value
-
-
-class TestDelete:
-    """Test the CauseCRUD.delete() method."""
-
-    def test_delete_cascades_to_bookmarks_but_not_causes(
-        self,
-        test_session: Session,
-    ):
-        """Deleting a cause should delete associated bookmarks, but not users."""
-        # set up - confirm alice exists
-        alice_id = uuid5(NAMESPACE, "alice")
-        assert test_session.get(User, alice_id) is not None
-        # setup - confirm cause exists
-        acme_id = uuid5(NAMESPACE, "acme")
-        assert test_session.get(Cause, acme_id) is not None
-        # set up - confirm bookmarks exist
-        bookmark_query = select(Bookmark).where(Bookmark.user_id == alice_id)
-        bookmarks_before = test_session.execute(bookmark_query).scalars().all()
-        assert len(bookmarks_before) == 1
-        # execution
-        user_service.delete(test_session, row_id=alice_id)
-        # validation - confirm user and bookmarks were deleted
-        bookmarks_after = test_session.execute(bookmark_query).scalars().all()
-        assert test_session.get(User, alice_id) is None
-        assert len(bookmarks_after) == 0
-        # validation - confirm cause was NOT deleted
-        assert test_session.get(Cause, acme_id) is not None
